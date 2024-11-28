c5c904fce7e9441e99e3c2326bdcf3125ca69318
Sumit-Singh94 <sumitsin3122@gmail.com>
2024-11-28 22:05:03 +0530

    Creating a portfolioWebsite

---
+++ Portfolio-Website/src/App.jsx
@@ -0,0 +1,18 @@
+
+import { Outlet } from "react-router-dom"
+import Home from "./Components/Home/Home"
+import Header from "./Components/Header/Header"
+import Footer from "./Components/Footer/Footer"
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+    <Home/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
