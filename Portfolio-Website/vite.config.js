fe9dccf0d7b055bcff22c740e84405619b43cbd9
Sumit-Singh94 <sumitsin3122@gmail.com>
2024-11-30 11:34:43 +0530

    updated

--- Portfolio-Website/vite.config.js
+++ Portfolio-Website/vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
+
 export default defineConfig({
-  plugins: [react()],
+  root: './',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: 'src/main.jsx'
+    }
+  }
 })
