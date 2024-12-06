553ca56064ddbdc5e6e8eb37fe6d2c2909501735
Sumit-Singh94 <sumitsin3122@gmail.com>
2024-12-06 21:49:36 +0530

    adding themeMode feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
-
 import { Outlet } from "react-router-dom"
+import {ThemeProvider} from "./context/theme"
 import Home from "./Components/Home/Home"
 import Header from "./Components/Header/Header"
 import Footer from "./Components/Footer/Footer"
+import { useState } from "react"
 
 function App() {
 
+  const [themeMode,setThemeMode]=useState("light")
+
+  const lightTheme=()=>{
+    setThemeMode("light")
+  }
+
+  const darkTheme=()=>{
+    setThemeMode("dark")
+
+  }
+
+
   return (
-    <>
+  
+    <ThemeProvider value={{themeMode,lightTheme,darkTheme}}>
     <Header/>
     <Outlet/>
     <Footer/>
-    </>
+    </ThemeProvider>
   )
 }
 
